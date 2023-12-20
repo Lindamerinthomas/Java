1fe74844fc497dee6229f04f8e9f2fd596c81f6c
Lindamerinthomas <129536010+Lindamerinthomas@users.noreply.github.com>
2023-12-20 16:30:55 -0500

    Update IfElseDemo.java

--- IfElseDemo.java
+++ IfElseDemo.java
@@ -12,3 +12,29 @@ class IfElseDemo{
      }
   }
 }
+
+
+(while loop)
+class Sample{
+  public static void main(String args[])
+  {
+  int i =0;
+  while(i<5) {
+   System.out.println("i:"+i);
+      i = i+1;
+  }
+  }
+}
+
+
+(Do while loop)
+class Sample{
+  public static void main(String args[])
+  {
+  int i =5;
+  do {
+   System.out.println("i:"+i);
+      i = i+1;
+  } while(i<5) 
+  }
+  }
