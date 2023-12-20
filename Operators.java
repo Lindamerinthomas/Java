53b492f4996cf24aeb845121ffb57b6873949440
Lindamerinthomas <129536010+Lindamerinthomas@users.noreply.github.com>
2023-12-20 15:51:13 -0500

    Update Operators.java

--- Operators.java
+++ Operators.java
@@ -7,3 +7,19 @@ System.out.println("a+b="+(a+b));
 System.out.println("a-b="+(a-b));
 }
 }
+
+
+class UnaryOperatorDemo{
+ public static void main(String args[])
+{
+int a = 10;
+int b = 15;
+int c = ++a;
+int d = b++;
+System.out.println("c:"+c);
+System.out.println("d:"+d);
+System.out.println("b:"+b);
+}
+}
+
+
