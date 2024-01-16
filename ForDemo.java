90cd3421302f8f2f049da928c37a3ff1d067a91c
Lindamerinthomas <129536010+Lindamerinthomas@users.noreply.github.com>
2024-01-16 07:06:47 -0500

    Update ForDemo.java

--- ForDemo.java
+++ ForDemo.java
@@ -1,9 +1,9 @@
 class ForDemo{
   public static void main(String args[])
   {
-     for(int i=1;i<=10;i++)
-     {
+      for(int i=1;i<=10;i++)
+      {
         System.out.println(i);
-     }
+      }
   }
 }
