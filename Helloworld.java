4f62d17d75e40cf7269309ba1651280a7f6e433b
Lindamerinthomas <129536010+Lindamerinthomas@users.noreply.github.com>
2024-01-16 07:08:09 -0500

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,7 @@
 Class Helloworld
 {
-  public static void main (String args[])
-  {
-    System.out.println("Hello World");
-  }
+   public static void main (String args[])
+   {
+     System.out.println("Hello World");
+   }
 }
